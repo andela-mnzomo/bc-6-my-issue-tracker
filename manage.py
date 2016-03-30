45dc6ce942024415628d7e8afbca23a046a28638
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-03-30 10:22:01 +0300

    Updated manage.py file

--- manage.py
+++ manage.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python
 import os
-from flask import Flask
+from app import create_app, db
+from app.models import User
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    '''Returns application and database instances
+    to the shell importing them automatically
+    on `python manager.py shell`.
+    '''
+    return dict(app=app, db=db, User=User)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
