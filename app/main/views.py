401732bbd0e7fe447346eb279876fe600d5b8370
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-03-30 11:15:17 +0300

    Add main module with views and forms

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, flash, redirect, request, url_for
+from . import main
+from .. import db
+
+@main.route('/')
+def home():
+    return render_template('dashboard.html', title= "Dashboard")
+
+@main.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', title="404 Error"), 404
+
+
