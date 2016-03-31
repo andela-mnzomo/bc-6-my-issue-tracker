b9ab6a582c4c184bfc086e74300b3dd15c7ef7b4
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-03-31 11:56:13 +0300

    Update issues views file to reflect changes in forms file

--- app/issues/views.py
+++ app/issues/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect, request, url_for
-from flask.ext.login import login_user, logout_user, login_required
+from flask import render_template, flash, redirect, request, url_for, abort
+from flask.ext.login import login_user, logout_user, login_required, current_user
+from datetime import datetime
 from . import issues
 from forms import IssueForm
+from ..models import Issue, User
 from .. import db
 
 @issues.route('/raise', methods=['GET', 'POST'])
@@ -12,7 +14,8 @@ def raise_issue():
 		issue = Issue(subject=form.subject.data,
                     description=form.description.data,
                     priority=form.priority.data,
-                    department=form.department.data)
+                    department_id=form.department.data,
+                    user=current_user._get_current_object())
 		db.session.add(issue)
 		db.session.commit()
 		flash('Thanks! We have recorded your issue.')
@@ -21,4 +24,51 @@ def raise_issue():
 @issues.route('/view')
 @login_required
 def view():
-    return render_template('issues/view.html', title = "My Issues")
+	issues = Issue.query.filter(Issue.user_id == current_user.id).all()
+	return render_template('issues/view.html', issues=issues, title="My Issues")
+
+@issues.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+	issue = Issue.query.get_or_404(id)
+	if current_user.id != issue.user_id:
+		abort(403)
+	form = IssueForm(obj=issue)
+	if form.validate_on_submit():
+		issue.subject = form.subject.data
+		issue.description = form.description.data
+		issue.priority = form.priority.data
+		issue.department_id = form.department.data
+		issue.user=current_user._get_current_object()
+		db.session.add(issue)
+		db.session.commit()
+		flash('Your issue has been updated.')
+	form.description.data = issue.description
+	form.subject.data = issue.subject
+	return render_template('issues/edit.html', form=form, title="Edit Issue")
+
+@issues.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+	issue = Issue.query.get_or_404(id)
+	if current_user.id != issue.user_id:
+		abort(403)
+	db.session.delete(issue)
+	return render_template('issues/delete.html', issue=issue, title="Delete Issue")
+
+@issues.route('/deleting/<int:id>', methods=['GET', 'POST'])
+@login_required
+def deleting(id):
+	issue = Issue.query.get_or_404(id)
+	db.session.delete(issue)
+	db.session.commit()
+	flash('Your issue has been deleted.')
+	return redirect(url_for('issues.view'))
+	return render_template('issues/delete.html', issue=issue)
+
+@issues.route('/admin/view')
+@login_required
+def admin_view():
+	# issues = Issue.query.filter(Issue.user_id == current_user.id).all()
+	issues = Issue.query.all()
+	return render_template('issues/view-admin.html', issues=issues, title="Issues Raised")
