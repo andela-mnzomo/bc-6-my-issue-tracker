1b4cec3b77695bbb02cad2e851712c67383ed82b
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-03-31 11:56:38 +0300

    Modify views for main module

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,15 @@
 from flask import render_template, flash, redirect, request, url_for
+from flask.ext.login import login_user, logout_user, login_required
 from . import main
 from .. import db
 
 @main.route('/')
-def home():
+def welcome():
+    return render_template('welcome.html', title= "Welcome")
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
     return render_template('dashboard.html', title= "Dashboard")
-
-
-@main.errorhandler(404)
-def not_found(error):
-    return render_template('404.html', title="404 Error"), 404
 
 
