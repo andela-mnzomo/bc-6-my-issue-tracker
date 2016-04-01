24b850dea51aeba1d4a876153d3bcb30dd3401fb
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-04-01 09:19:14 +0300

    Add count to dashboard

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,30 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import main
 from .. import db
+from ..models import Issue
 
 @main.route('/')
 def welcome():
-    return render_template('welcome.html', title= "Welcome")
+    return render_template('welcome.html', title="Welcome")
 
 @main.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', title= "Dashboard")
+    issue_count = Issue.query.filter(Issue.user_id == current_user.id).count()
+    resolved_issues = (Issue.query
+        .filter(Issue.user_id == current_user.id)
+        .filter(Issue.is_resolved == True)
+        ).count()
+    issues_in_progress = (Issue.query
+        .filter(Issue.user_id == current_user.id)
+        .filter(Issue.is_assigned == True)
+        ).count()
+
+    counts = dict(issue_count=issue_count, resolved_issues=resolved_issues, issues_in_progress=issues_in_progress)
+    return render_template('dashboard.html', counts=counts, title="Dashboard")
+
+
+
 
 
